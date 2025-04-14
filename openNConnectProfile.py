30ae526662322b8bdbb8aec8fc8092045831f2f7
unknown <jitendranathswamijsm@gmail.com>
2025-04-14 14:17:31 +0530

    Reading task and profileNumber from data.json

--- openNConnectProfile.py
+++ openNConnectProfile.py
@@ -3,16 +3,23 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use import Agent, Browser, BrowserConfig
 from pydantic import SecretStr
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
+
 import asyncio
+
+
+with open("data.json","r") as data_file:
+    data_dict=json.load(data_file)
+
 
 # Configure the browser to connect to your Chrome instance with Profile 2
 browser = Browser(
     config=BrowserConfig(
         chrome_instance_path='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        extra_chromium_args=['--profile-directory=Profile 2']
+        extra_chromium_args=[f'--profile-directory=Profile {data_dict['profileNumber']}']
     )
 )
 
@@ -21,14 +28,14 @@ api_key = os.getenv("GEMINI_API_KEY")
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(os.getenv('GEMINI_API_KEY')))
 
 async def main():
-    task = 'Check URL of all opened tabs is there any tab with url "chrome://privacy-sandbox-dialog/notice" if yes then click on button having text "Got it"'
-    task += 'Open https://www.youtube.com'
-    task += 'Search for "top 10 movies"'
-    task += 'Click on the first video not a youtube short video'
-    task += 'Give me the first 10 comments in list format'
+    # task = 'Check URL of all opened tabs is there any tab with url "chrome://privacy-sandbox-dialog/notice" if yes then click on button having text "Got it"'
+    # task += 'Open https://www.youtube.com'
+    # task += 'Search for "top 10 movies"'
+    # task += 'Click on the first video not a youtube short video'
+    # task += 'Give me the first 10 comments in list format'
     # Create agent with the model
     agent = Agent(
-        task=task,
+        task=data_dict['task'],
         llm=llm,
         browser=browser
     )
